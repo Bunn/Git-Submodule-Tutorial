7c9cea3930562f57a59be405b0075735fbf94b27
Fernando Bunn <bunn@idevzilla.com>
2015-09-20 16:26:29 -0300

    Add empty xcode project

---
+++ SubmoduleTutorial/SubmoduleTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SubmoduleTutorial
+//
+//  Created by Fernando Bunn on 9/20/15.
+//  Copyright © 2015 iDevzilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
